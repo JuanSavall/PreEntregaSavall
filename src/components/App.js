d41d2cd7a2a6f02efb0b4d1d8c72649bbd638e1d
chino <chino.savall@outlook.com>
2023-12-10 14:05:21 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar/NavBar";
+
+function app () {
+    return(
+        <div className="App">
+            <NavBar />    
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Toallitas.coco</h3>
+            <div>
+                <button>kit inicial</button>
+                <button>kit regular</button>
+                <button>kit diario</button>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar
